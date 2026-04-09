e968c6b33078a6b3998c082b1aac5f714524fe93
nolan <272226345+NolanJX@users.noreply.github.com>
2026-04-09 21:35:35 +0800

    chore: add flex-center shortcuts to UnoCSS config

--- uno.config.ts
+++ uno.config.ts
@@ -2,4 +2,8 @@ import { defineConfig, presetWind4 } from "unocss";
 
 export default defineConfig({
   presets: [presetWind4()],
+  shortcuts: {
+    "flex-center": "flex justify-center items-center",
+    "flex-col-center": "flex flex-col justify-center items-center",
+  },
 });
