59524018323a9293e1f317f95657f2c943037c24
nolan <272226345+NolanJX@users.noreply.github.com>
2026-04-18 22:42:04 +0800

    fix: add tabs permission to resolve popup URL error in production builds

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    permissions: ["storage"],
+    permissions: ["storage", "tabs"],
   },
   modules: ["@wxt-dev/module-vue", "@wxt-dev/unocss"],
   unocss: {
